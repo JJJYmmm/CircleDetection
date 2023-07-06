e04c58109cf7829806651ef30433c0d19b33bd4e
JJJYmmm <1650675829@qq.com>
2023-07-06 20:08:11 +0800

    image update

--- img_concat.py
+++ img_concat.py
@@ -1,9 +1,14 @@
 import cv2
 import numpy as np
+
 # img1 = cv2.imdecode(np.fromfile('./picture_source/picture.jpg', dtype=np.uint8), -1)
-img1 = cv2.imdecode(np.fromfile('./picture_result/canny_result.jpg', dtype=np.uint8), -1)
+img1 = cv2.imdecode(
+    np.fromfile("./picture_result/canny_result.jpg", dtype=np.uint8), -1
+)
 img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
-img2 = cv2.imdecode(np.fromfile('./picture_result/hough_result.jpg', dtype=np.uint8), -1)
+img2 = cv2.imdecode(
+    np.fromfile("./picture_result/hough_result.jpg", dtype=np.uint8), -1
+)
 # 纵向合并
 print(img1.shape)
 print(img2.shape)
